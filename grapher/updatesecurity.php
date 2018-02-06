16c1dcdc9c99fa5bbca33af877da6b6b1e1b7228
mathsnz <jwills@mathsnz.com>
2018-02-06 14:26:21 +1300

    update

--- grapher/updatesecurity.php
+++ grapher/updatesecurity.php
@@ -5,6 +5,9 @@ $files = glob("./*/password.php");
 foreach ($files as $file){
 	$dir = dirname($file);
 	$password = file_get_contents($file);
+	if(substr($password,0,1)=="<"){
+		continue;
+	}
 	file_put_contents($file,"<?php \$correctpass='$password'; ?>");
 	file_put_contents($dir."/index.php",file_get_contents('./updatesecuritynewindex.php'));
 }
