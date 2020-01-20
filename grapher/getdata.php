fe399dccefa7a7ddf7258cb38b0253f3448a98f0
mathsnz <jwills@mathsnz.com>
2020-01-21 10:07:46 +1300

    update

--- grapher/getdata.php
+++ grapher/getdata.php
@@ -1,7 +1,18 @@
 <?php
 $url = urldecode($_GET['dataset']);
 $file_headers = @get_headers($url);
-if($file_headers[0]!="HTTP/1.1 200 OK"){
+// we want the the last errorcode, reverse array so we start at the end:
+$file_headers = array_reverse($file_headers);
+$code = "";
+foreach($file_headers as $hline){
+	// search for things like "HTTP/1.1 200 OK" , "HTTP/1.0 200 OK" , "HTTP/1.1 301 PERMANENTLY MOVED" , "HTTP/1.1 400 Not Found" , etc.
+	// note that the exact syntax/version/output differs, so there is some string magic involved here
+	if(preg_match('/^HTTP\/\S+\s+([1-9][0-9][0-9])\s+.*/', $hline, $matches) ){// "HTTP/*** ### ***"
+		$code = $matches[1];
+		break;
+	}
+}
+if($code!="200"){
 	echo "Error\r\n";
 	print_r($file_headers);
 } else {
