b4464b5c4a7c1d1198af20e88db0d292b3544697
mathsnz <jwills@mathsnz.com>
2020-01-21 09:51:40 +1300

    update

--- grapher/getdata.php
+++ grapher/getdata.php
@@ -1,6 +1,6 @@
 <?php
 $url = urldecode($_GET['dataset']);
-$file_headers = get_headers($url);
+$file_headers = @get_headers($url);
 if($file_headers[0]!="HTTP/1.1 200 OK"){
 	echo "Error\r\n";
 	print_r($file_headers);
